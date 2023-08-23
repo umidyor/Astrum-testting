aa2b2db269da75588e687a50f33d7f1f8833465c
Student <student@iMac-Astrum-53.local>
2023-08-23 18:31:31 +0500

    fixed index

--- static/js/index.js
+++ static/js/index.js
@@ -3,16 +3,16 @@ function redirectTo(action) {
 
     if (isLoggedIn) {
         if (action === 'create_test') {
-            var button = document.getElementById("createTestButton");
-            if (button) {
-                button.style.backgroundColor = "#A446E2"; // Change color on click
-            }
+            // var button = document.getElementById("createTestButton");
+            // if (button) {
+            //     button.style.backgroundColor = "#A446E2"; // Change color on click
+            // }
             window.location.href = '/create_test'; // Redirect to create_test URL
         } else if (action === 'create_questionnaire') {
-            var button = document.getElementById("createQuestionnaireButton");
-            if (button) {
-                button.style.backgroundColor = "#A446E2"; // Change color on click
-            }
+            // var button = document.getElementById("createQuestionnaireButton");
+            // if (button) {
+            //     button.style.backgroundColor = "#A446E2"; // Change color on click
+            // }
             window.location.href = '/blog'; // Redirect to blog page
         }
     } else {
